3471eed582b718bfb382bf1b02c681ac1893892d
MikhaiP <123189849+MikhaiP@users.noreply.github.com>
2024-11-20 21:13:52 -0400

    Third Commit
    Works sending info

--- app/src/main/java/com/example/publisher/MainActivity.kt
+++ app/src/main/java/com/example/publisher/MainActivity.kt
@@ -6,23 +6,35 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+//import android.location.LocationRequest
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client
 import java.util.UUID
 import com.google.gson.Gson
+import org.json.JSONObject
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationRequest
 
 
-class MainActivity : AppCompatActivity(){
+
+class MainActivity : AppCompatActivity() {
     private var client: Mqtt5BlockingClient? = null
     private var startPublishing: Boolean = false
     private lateinit var studentIDEditText: EditText
@@ -32,6 +44,7 @@ class MainActivity : AppCompatActivity(){
     private var speed: Double = 0.0
     private var latitude: Double = 0.0
     private var longitude: Double = 0.0
+    private lateinit var locationAction: LocationCallback
 
     data class LocationData(
         val StudentID: String,
@@ -43,9 +56,13 @@ class MainActivity : AppCompatActivity(){
     )
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
 
     private val request = 1234
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -63,6 +80,11 @@ class MainActivity : AppCompatActivity(){
             .build()
             .toBlocking()
 
+        locationAction = object : LocationCallback() {
+            override fun onLocationResult(locationR: LocationResult) {
+                runPublishLocation(locationR)
+            }
+        }
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationListener = object : LocationListener{
             override fun onLocationChanged(location: Location) {
@@ -72,40 +94,56 @@ class MainActivity : AppCompatActivity(){
             }
 
         }
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-//            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
-//                request
-//            )
-//        } else {
-//            // Start location updates if permissions are already granted
-//            startLocationUpdates()
-//        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                request
+            )
+        } else {
+            // Start location updates if permissions are already granted
+            startLocationUpdates()
+        }
 //        val isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
 //        if (!isGPSEnabled) {
 //            Toast.makeText(this, "Please enable GPS", Toast.LENGTH_SHORT).show()
 //            Log.d("LocationUpdates", "GPS provider is disabled")
 //        }
-    }
-//    private fun startLocationUpdates() {
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-//            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-//
-//            locationManager.requestLocationUpdates(
-//                LocationManager.GPS_PROVIDER,
-//                1000L,  // Update interval in milliseconds
-//                1f,     // Minimum distance in meters
-//                locationListener
-//            )
-//            Log.d("LocationUpdates", "Started location updates")
-//        } else {
-//            Log.d("LocationUpdates", "Permissions not granted")
-//        }
-//    }
 
+    }
+    fun runPublishLocation(locationResult: LocationResult){
+        locationResult.locations.forEach{location ->
+            publishLocationToBroker(location)
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun startLocationUpdates() {
+//        var locationRequest: LocationRequest
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        val locationRequest = LocationRequest.Builder(5000)
+                .setMinUpdateDistanceMeters(5f)
+                .setMinUpdateIntervalMillis(2000)
+                .build()
+
+
+        fusedLocationClient.requestLocationUpdates(locationRequest,locationAction,Looper.getMainLooper())
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1000L,  // Update interval in milliseconds
+                1f,     // Minimum distance in meters
+                locationListener
+            )
+            Log.d("LocationUpdates", "Started location updates")
+
+
+    }
 
     fun publishToBroker(view: View?) {
         if (startPublishing) {
@@ -221,15 +259,16 @@ class MainActivity : AppCompatActivity(){
 
     private fun publishLocationToBroker(location: Location){
         studentID = studentIDEditText.text.toString()
-//        val locationData = "StudentID: $studentID, ID: $id, Time: $ms_time, Speed: $speed km/h, Latitude: $latitude, Longitude: $longitude"
-//        Log.d("Data", "StudentID: $studentID, ID: $id, Time: $ms_time, Speed: $speed km/h, Latitude: $latitude, Longitude: $longitude")
+////        val locationData = "StudentID: $studentID, ID: $id, Time: $ms_time, Speed: $speed km/h, Latitude: $latitude, Longitude: $longitude"
+////        Log.d("Data", "StudentID: $studentID, ID: $id, Time: $ms_time, Speed: $speed km/h, Latitude: $latitude, Longitude: $longitude")
 //
-        id += 1u
+//        id += 1u
         ms_time = System.currentTimeMillis()
-        speed = location.speed * 3.6
+//        speed = location.speed * 3.6
         latitude = location.latitude
         longitude = location.longitude
 
+        Log.d("Location Update", "$studentID, $ms_time, $latitude, $longitude")
         val locationData = LocationData(
             StudentID = studentID,
 //            ID = "$id",
@@ -238,7 +277,7 @@ class MainActivity : AppCompatActivity(){
             Latitude = latitude,
             Longitude = longitude
         )
-
+        val test = "Test"
         val gson = Gson()
         val locationDataJson = gson.toJson(locationData)
 
@@ -250,6 +289,7 @@ class MainActivity : AppCompatActivity(){
         } catch (e:Exception){
             Toast.makeText(this,"An error occurred when sending a message to the broker", Toast.LENGTH_SHORT).show()
         }
+
     }
 
 
